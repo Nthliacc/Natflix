d315910e62487938bfd0c0542aadbe42a355d9ad
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-08-05 09:45:35 -0300

    organizando pages de rotas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Home from "./pages/home";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CadastroVideo from "./pages/cadastro";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
